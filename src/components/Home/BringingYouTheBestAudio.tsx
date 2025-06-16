ca5a7c3ded4fbeb7641a54e5fa98d556d544346f
Trinity6264 <amoahtnt6@gmail.com>
2025-06-16 23:27:10 +0000

    Enhance text styling in BringingYouTheBestAudio component for improved readability

--- src/components/Home/BringingYouTheBestAudio.tsx
+++ src/components/Home/BringingYouTheBestAudio.tsx
@@ -9,7 +9,7 @@ const BringingYouTheBestAudio = () => {
                 </div>
                 <div className="text-center md:text-left">
                     <h2 className="text-[28px] font-bold text-center uppercase">Bringing you the <span className="text-orange-500">best</span> audio gear</h2>
-                    <p className="mt-8 text-[#000] opacity-50">
+                    <p className="mt-8 text-[#000] font-medium text-[15px] text-center opacity-50">
                         Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
                     </p>
                 </div>
