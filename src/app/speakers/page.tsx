a3396b48369b108beeac185de25df6e2f1cc7895
Trinity6264 <amoahtnt6@gmail.com>
2025-06-17 11:00:06 +0000

    Add ProductCategoryCard component to Speakers page for better product categorization

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,3 +1,4 @@
+import ProductCategoryCard from '@/components/Home/ProductCategoryCard'
 import NewProductCard from '@/components/NewProductCard'
 import PageTitle from '@/components/PageTitle'
 
@@ -10,6 +11,13 @@ const page = () => {
         <NewProductCard description='Upgrade your sound system with the all new ZX9 active speaker. It&apos;s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.' imagePathLink='/assets/product-zx9-speaker/mobile/image-product.jpg' title='NEW PRODUCT' subTitle='YX1 WIRELESS EARPHONES' productPath=''/>
         
         <NewProductCard description='Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.' imagePathLink='/assets/product-zx7-speaker/mobile/image-product.jpg'  subTitle='ZX7 SPEAKER' productPath=''/>
+        
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-36">
+          <ProductCategoryCard name="Headphones" image="/assets/shared/desktop/image-category-thumbnail-headphones.png" path={'/headphones'} />
+          <ProductCategoryCard name="Speakers" image="/assets/shared/desktop/image-category-thumbnail-speakers.png" path={'/speakers'} />
+          <ProductCategoryCard name="Earphones" image="/assets/shared/desktop/image-category-thumbnail-earphones.png" path={'/earphones'} />
+        </div>
+        
       </div>
     </div>
   )
--- src/components/Home/ProductCategoryCard.tsx
+++ src/components/Home/ProductCategoryCard.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Image from "next/image"
 import { useRouter } from "next/navigation";
 import { FC } from "react";
+
 
 
 interface CategoryCardProps {
@@ -19,8 +22,8 @@ const ProductCategoryCard:FC<CategoryCardProps> = ({ name, image, path }) => {
           router.push(path)
       }}>
           <Image src={image} width={100} height={100} alt={name} className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-40 h-auto group-hover:scale-110 transition-transform duration-300" />
-          <h3 className="text-lg font-bold uppercase">{name}</h3>
-          <button className="mt-4 text-gray-500 uppercase flex items-center gap-2 group-hover:text-orange-500 transition-colors cursor-pointer">
+          <h3 className="text-[15px] text-[#000] font-bold uppercase ">{name}</h3>
+          <button className="mt-4 text-[#000] opacity-50 font-bold flex items-center gap-2 group-hover:text-orange-500 transition-colors cursor-pointer">
               Shop <svg className="w-4 h-4 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
           </button>
       </div>
